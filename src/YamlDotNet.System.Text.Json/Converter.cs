e470ea235ba927de7e6058e4cd30aab249cdc1af
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-08-09 09:08:48 -0700

    feat: Initial Build (#1)

---
+++ src/YamlDotNet.System.Text.Json/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using YamlDotNet.Serialization;
+
+namespace YamlDotNet.System.Text.Json
+{
+    public static class YamlConverter
+    {
+        public static ISerializer DefaultSerializer = new SerializerBuilder()
+                .DisableAliases()
+                .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull | DefaultValuesHandling.OmitDefaults)
+                .WithTypeConverter(new JsonNodeYamlConverter())
+                .Build();
+
+        public static IDeserializer DefaultDeserializer = new DeserializerBuilder()
+                .WithTypeConverter(new JsonNodeYamlConverter())
+                .Build();
+
+        public static string Serialize(JsonNode jsonNode, ISerializer? serializer = null)
+        {
+            serializer ??= DefaultSerializer;
+            return serializer.Serialize(jsonNode);
+        }
+
+        public static JsonNode Deserialize(string yaml, IDeserializer? deserializer = null)
+        {
+            deserializer ??= DefaultDeserializer;
+            return deserializer.Deserialize<JsonNode>(yaml);
+        }
+    }
+}
---
+++ src/YamlDotNet.System.Text.Json/JsonNodeYamlConverter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Core.Tokens;
+using YamlDotNet.Serialization;
+using Scalar = YamlDotNet.Core.Events.Scalar;
+
+namespace YamlDotNet.System.Text.Json
+{
+    public sealed class JsonNodeYamlConverter : IYamlTypeConverter
+    {
+        public bool Accepts(Type type)
+        {
+            return type == typeof(JsonNode) || type == typeof(JsonArray) || type == typeof(JsonObject) || type == typeof(JsonValue) || type?.BaseType == typeof(JsonValue) || type.BaseType?.BaseType == typeof(JsonValue);
+        }
+
+        public object? ReadYaml(IParser parser, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteYaml(IEmitter emitter, object? value, Type type)
+        {
+            if (type == typeof(JsonValue) || type?.BaseType == typeof(JsonValue) || type.BaseType?.BaseType == typeof(JsonValue))
+            {
+                WriteValue(emitter, value);
+            }
+            else if (type == typeof(JsonObject) || type?.BaseType == typeof(JsonObject) || type?.BaseType?.BaseType == typeof(JsonObject))
+            {
+                WriteObject(emitter, value);
+            }
+            else if (type == typeof(JsonArray) || type?.BaseType == typeof(JsonArray) || type?.BaseType?.BaseType == typeof(JsonArray))
+            {
+                WriteArray(emitter, value);
+            }
+            else
+            {
+                //Shouldn't be here!
+            }
+        }
+
+        private void WriteObject(IEmitter emitter, object value)
+        {
+            emitter.Emit(new MappingStart(null, null, false, MappingStyle.Any));
+
+            foreach (var property in (JsonObject)value)
+            {
+                JsonNode propVal = property.Value;
+
+                emitter.Emit(new Scalar(null, property.Key));
+
+                WriteYaml(emitter, propVal, propVal.GetType());
+            }
+
+            emitter.Emit(new MappingEnd());
+        }
+
+        private void WriteValue(IEmitter emitter, object value)
+        {
+            JsonNode obj = (JsonNode)value;
+
+            emitter.Emit(new Scalar(obj.ToString()));
+        }
+
+        private void WriteArray(IEmitter emitter, object value)
+        {
+            var style = SequenceStyle.Any;
+
+            emitter.Emit(new SequenceStart(null, null, false, style));
+
+            foreach (var item in ((JsonArray)value))
+            {
+                WriteYaml(emitter, item, item.GetType());
+            }
+
+            emitter.Emit(new SequenceEnd());
+        }
+    }
+}
