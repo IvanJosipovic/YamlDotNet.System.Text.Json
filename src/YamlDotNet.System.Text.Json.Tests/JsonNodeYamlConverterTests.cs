d63c0bc468b8e90782935b657ab57d4038dc70dc
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-08-08 21:25:42 -0700

    wip

---
+++ src/YamlDotNet.System.Text.Json.Tests/JsonNodeYamlConverterTests.cs
@@ -0,0 +1,35 @@
+using System.Xml;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace YamlDotNet.System.Text.Json.Tests
+{
+    public class JsonNodeYamlConverterTests
+    {
+        [Theory]
+        [InlineData("{\"Temperature\": \"25\"}")]
+        //[InlineData("{\"Temperature\": 25}")]
+        //[InlineData("{\"Temperatures\": [1,2,3]}")]
+
+        //[InlineData("25")]
+
+        //[InlineData("test\ntest2\ntest3")]
+        //[InlineData("")]
+        //[InlineData("true")]
+        //[InlineData("false")]
+        //[InlineData("0")]
+        //[InlineData("100")]
+        //[InlineData("1.55f")]
+
+        public void Tests(string val)
+        {
+            JsonNode val2 = JsonNode.Parse(val);
+
+            string yaml = YamlConverter.Serialize(val2);
+
+            JsonNode val3 = YamlConverter.Deserialize(yaml);
+
+            Assert.Equal(val2.ToJsonString(), val3.ToJsonString());
+        }
+    }
+}
---
+++ src/YamlDotNet.System.Text.Json/JsonNodeYamlConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Core.Tokens;
+using YamlDotNet.Serialization;
+using Scalar = YamlDotNet.Core.Events.Scalar;
+
+namespace YamlDotNet.System.Text.Json
+{
+    public sealed class JsonNodeYamlConverter : IYamlTypeConverter
+    {
+        public bool Accepts(Type type)
+        {
+            return type == typeof(JsonNode) || type == typeof(JsonArray) || type == typeof(JsonObject) || type == typeof(JsonValue) || type.BaseType == typeof(JsonValue);
+        }
+
+        public object? ReadYaml(IParser parser, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteYaml(IEmitter emitter, object? value, Type type)
+        {
+            if (type == typeof(JsonValue) || type.BaseType == typeof(JsonValue))
+            {
+                WriteValue(emitter, value);
+            }
+            else if (type == typeof(JsonObject) || type.BaseType == typeof(JsonObject))
+            {
+                WriteObject(emitter, value);
+            }
+            else if (type == typeof(JsonArray) || type.BaseType == typeof(JsonArray))
+            {
+                WriteArray(emitter, value);
+            }
+        }
+
+        private void WriteObject(IEmitter emitter, object value)
+        {
+            emitter.Emit(new MappingStart(null, null, false, MappingStyle.Any));
+
+            JsonObject obj = (JsonObject)value;
+
+            foreach (var property in obj)
+            {
+                JsonNode propVal = property.Value;
+                emitter.Emit(new Scalar(null, property.Key));
+                WriteYaml(emitter, propVal, propVal.GetType());
+            }
+
+            emitter.Emit(new MappingEnd());
+        }
+
+        private void WriteValue(IEmitter emitter, object value)
+        {
+
+        }
+
+        private void WriteArray(IEmitter emitter, object value)
+        {
+            var style = SequenceStyle.Any;
+
+            emitter.Emit(new SequenceStart(null, null, false, style));
+
+            foreach (var item in ((JsonArray)value))
+            {
+                WriteYaml(emitter, item, item.GetType());
+            }
+
+            emitter.Emit(new SequenceEnd());
+        }
+    }
+}
