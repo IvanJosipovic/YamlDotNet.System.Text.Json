de1313f68695a4fdfbed1736afe379a621df4096
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-11-09 23:53:56 -0800

    chore: Increase coverage for System.Text.Json integration (#165)

---
+++ src/YamlDotNet.System.Text.Json.Tests/SystemTextJsonTypeInspectorTests.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Shouldly;
+using YamlDotNet.Serialization;
+
+namespace YamlDotNet.System.Text.Json.Tests;
+
+public class SystemTextJsonTypeInspectorTests
+{
+    [Fact]
+    public void GetProperty_UsesCaseInsensitiveMatching()
+    {
+        var descriptors = new[]
+        {
+            new TestPropertyDescriptor("ActualName", typeof(string))
+        };
+
+        var inspector = new SystemTextJsonTypeInspector(new StubTypeInspector(descriptors));
+
+        var result = inspector.GetProperty(typeof(object), null, "actualname", ignoreUnmatched: true, caseInsensitivePropertyMatching: true);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe("ActualName");
+    }
+
+    [Fact]
+    public void GetProperty_ReturnsExtensionDescriptorWhenMissing()
+    {
+        var container = new ContainerWithExtensionData();
+        container.ExtensionData["dynamic"] = JsonSerializer.SerializeToElement("value");
+
+        var attributes = new Attribute[] { new JsonExtensionDataAttribute() };
+        var extensionDescriptor = new TestPropertyDescriptor(
+            "ExtensionData",
+            typeof(Dictionary<string, JsonElement>),
+            reader: _ => container.ExtensionData,
+            writer: (_, value) =>
+            {
+                if (value is IDictionary<string, JsonElement> dict)
+                {
+                    container.ExtensionData = dict;
+                }
+            },
+            attributes: attributes);
+
+        var inspector = new SystemTextJsonTypeInspector(new StubTypeInspector(new[] { extensionDescriptor }));
+
+        var result = inspector.GetProperty(typeof(ContainerWithExtensionData), container, "newKey", ignoreUnmatched: true, caseInsensitivePropertyMatching: false);
+
+        result.ShouldBeOfType<ExtensionDataPropertyDescriptor>();
+        result.Name.ShouldBe("newKey");
+    }
+
+    [Fact]
+    public void GetProperty_ThrowsWhenMultipleMatches()
+    {
+        var descriptors = new[]
+        {
+            new TestPropertyDescriptor("Duplicate", typeof(string)),
+            new TestPropertyDescriptor("Duplicate", typeof(string))
+        };
+
+        var inspector = new SystemTextJsonTypeInspector(new StubTypeInspector(descriptors));
+
+        Should.Throw<SerializationException>(() => inspector.GetProperty(typeof(object), null, "Duplicate", ignoreUnmatched: true, caseInsensitivePropertyMatching: false));
+    }
+
+    private sealed class StubTypeInspector : ITypeInspector
+    {
+        private readonly IReadOnlyList<IPropertyDescriptor> _properties;
+
+        public StubTypeInspector(IReadOnlyList<IPropertyDescriptor> properties)
+        {
+            _properties = properties;
+        }
+
+        public string GetEnumName(Type enumType, string name) => name;
+
+        public string GetEnumValue(object enumValue) => enumValue.ToString()!;
+
+        public IEnumerable<IPropertyDescriptor> GetProperties(Type type, object? container) => _properties;
+
+        public IPropertyDescriptor GetProperty(Type type, object? container, string name, bool ignoreUnmatched, bool caseInsensitivePropertyMatching)
+        {
+            if (caseInsensitivePropertyMatching)
+            {
+                return _properties.First(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return _properties.First(p => p.Name == name);
+        }
+    }
+
+    private sealed class ContainerWithExtensionData
+    {
+        public IDictionary<string, JsonElement> ExtensionData { get; set; } = new Dictionary<string, JsonElement>();
+    }
+}
---
+++ src/YamlDotNet.System.Text.Json.Tests/SystemTextJsonYamlTypeConverterCoverageTests.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Shouldly;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.System.Text.Json;
+
+namespace YamlDotNet.System.Text.Json.Tests;
+
+public class SystemTextJsonYamlTypeConverterCoverageTests
+{
+    private static readonly ObjectDeserializer RootDeserializer = _ => null;
+    private static readonly ObjectSerializer StubSerializer = (_, _) => { };
+
+    [Fact]
+    public void ReadYaml_ReturnsNullForUnsupportedType()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var parser = CreateParser("value");
+
+        var result = converter.ReadYaml(parser, typeof(Guid), RootDeserializer);
+
+        result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ReadYaml_ReturnsNullForJsonObjectWhenScalarProvided()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var parser = CreateParser("scalar");
+
+        var result = converter.ReadYaml(parser, typeof(JsonObject), RootDeserializer);
+
+        result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ReadYaml_ReturnsNullForJsonNodeWhenEmpty()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var parser = CreateParser(string.Empty);
+
+        var result = converter.ReadYaml(parser, typeof(JsonNode), RootDeserializer);
+
+        result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ReadYaml_ReturnsNullForJsonDocumentWhenEmpty()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var parser = CreateParser(string.Empty);
+
+        var result = converter.ReadYaml(parser, typeof(JsonDocument), RootDeserializer);
+
+        result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void WriteYaml_HandlesJsonNodeValues()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var emitter = new RecordingEmitter();
+
+        converter.WriteYaml(emitter, JsonNode.Parse("{\"name\":1}")!, typeof(JsonNode), StubSerializer);
+        converter.WriteYaml(emitter, JsonNode.Parse("[1,2]")!, typeof(JsonNode), StubSerializer);
+        converter.WriteYaml(emitter, JsonNode.Parse("\"text\"")!, typeof(JsonNode), StubSerializer);
+
+        emitter.Events.OfType<MappingStart>().ShouldNotBeEmpty();
+        emitter.Events.OfType<SequenceStart>().ShouldNotBeEmpty();
+        emitter.Events.OfType<Scalar>().Any(s => s.Value == "text").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void WriteYaml_HandlesJsonElementKinds()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var emitter = new RecordingEmitter();
+
+        using var objectDoc = JsonDocument.Parse("{\"value\":1}");
+        converter.WriteYaml(emitter, objectDoc.RootElement, typeof(JsonElement), StubSerializer);
+
+        using var arrayDoc = JsonDocument.Parse("[1,2]");
+        converter.WriteYaml(emitter, arrayDoc.RootElement, typeof(JsonElement), StubSerializer);
+
+        using var stringDoc = JsonDocument.Parse("\"value\"");
+        converter.WriteYaml(emitter, stringDoc.RootElement, typeof(JsonElement), StubSerializer);
+
+        converter.WriteYaml(emitter, default(JsonElement), typeof(JsonElement), StubSerializer);
+
+        emitter.Events.OfType<Scalar>().ShouldNotBeEmpty();
+    }
+
+    [Fact]
+    public void WriteYaml_HandlesJsonValueKinds()
+    {
+        var converter = new SystemTextJsonYamlTypeConverter();
+        var emitter = new RecordingEmitter();
+
+        var stringValue = JsonValue.Create("value")!;
+        converter.WriteYaml(emitter, stringValue, typeof(JsonValue), StubSerializer);
+
+        var numberValue = JsonValue.Create(3.14)!;
+        converter.WriteYaml(emitter, numberValue, typeof(JsonValue), StubSerializer);
+
+        var boolValue = JsonValue.Create(true)!;
+        converter.WriteYaml(emitter, boolValue, typeof(JsonValue), StubSerializer);
+
+        var nullValue = JsonValue.Create((string?)null)!;
+        converter.WriteYaml(emitter, nullValue, typeof(JsonValue), StubSerializer);
+
+        var undefinedValue = (JsonValue)JsonValue.Create(default(JsonElement))!;
+        converter.WriteYaml(emitter, undefinedValue, typeof(JsonValue), StubSerializer);
+
+        emitter.Events.Count.ShouldBeGreaterThan(0);
+    }
+
+    private static Parser CreateParser(string yaml)
+    {
+        var reader = new Parser(new StringReader(yaml ?? string.Empty));
+        return reader;
+    }
+
+    private sealed class RecordingEmitter : IEmitter
+    {
+        public IList<ParsingEvent> Events { get; } = new List<ParsingEvent>();
+
+        public void Emit(ParsingEvent @event)
+        {
+            Events.Add(@event);
+        }
+    }
+}
